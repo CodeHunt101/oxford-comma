8428aa6aac27944b56dab5a371ef1d26d6e6e974
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-12 18:28:03 +1000

    Lab completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    i = array.size - 1
+    until i==0 do
+      array[i] = " #{array[i]}"
+      i-=1
+    end
+    array.join(",")
+  end
 end
